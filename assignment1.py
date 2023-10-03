ada67ab7a37c055c9cf87a484d4925afd2162b41
Ben Smith <tazzben@users.noreply.github.com>
2023-10-03 14:24:19 -0500

    Update assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import plotly.express as px
+from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing
+ 
+data = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/Assignment2/assignment2.csv")
+ 
+#check type and convert "Timestamp" to pd datetime
+data['Timestamp'] = pd.to_datetime(data['Timestamp'])
+ 
+#Set index and variable 'trips'
+x = data['trips']
+x.index = data['Timestamp']
+x.index.freq = x.index.inferred_freq
+ 
+#model specification
+model = ExponentialSmoothing(x, trend='add', seasonal='mul', damped_trend=True)
+modelFit = model.fit()
+pred=modelFit.forecast(744)
